017a807e673d2189edb5e89a8236368179a291b7
kaua <kauabenjamint@gmail.com>
2025-03-28 16:13:41 -0300

    ..

--- MinhasCompras/Views/ListaProduto.xaml.cs
+++ MinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,5 +1,6 @@
 using MinhasCompras.Models;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace MinhasCompras.Views;
 
@@ -12,23 +13,29 @@ public partial class ListaProduto : ContentPage
 	{
 		InitializeComponent();
 
-		lst_produtos.ItemsSource = listaFiltrada;
+        lst_produtos.ItemsSource = listaFiltrada;
 	}
 
     protected async override void OnAppearing()
 	{
-        //evitando as duplicações
-        listaCompleta.Clear();
-        listaFiltrada.Clear();
-
-        List<Produto> tmp = await App.DB.GetAll();
-
-        foreach (var item in tmp)
+        try
         {
-            listaCompleta.Add(item);
-            listaFiltrada.Add(item);
-        }
+            //evitando as duplicações
+            listaCompleta.Clear();
+            listaFiltrada.Clear();
 
+            List<Produto> tmp = await App.DB.GetAll();
+
+            foreach (var item in tmp)
+            {
+                listaCompleta.Add(item);
+                listaFiltrada.Add(item);
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }	
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
@@ -45,32 +52,73 @@ public partial class ListaProduto : ContentPage
 
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
-		string q = e.NewTextValue;
-
-		listaFiltrada.Clear();
-
-        List<Produto> tmp = await App.DB.Search(q);
-
-        foreach (var produto in listaCompleta)
+        try
         {
-            if (produto.Descricao.ToLower().Contains(q))
+            string q = e.NewTextValue;
+
+            listaFiltrada.Clear();
+
+            List<Produto> tmp = await App.DB.Search(q);
+
+            foreach (var produto in listaCompleta)
             {
-                listaFiltrada.Add(produto);
+                if (produto.Descricao.ToLower().Contains(q))
+                {
+                    listaFiltrada.Add(produto);
+                }
             }
         }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+
+
     }
 
     private void ToolbarItem_Clicked_1(object sender, EventArgs e)
     {
-		double soma = listaFiltrada.Sum(i => i.Total);
+        try
+        {
+            double soma = listaFiltrada.Sum(i => i.Total);
 
-		string msg = $"O total é: {soma:C}";
+            string msg = $"O total é: {soma:C}";
 
-		DisplayAlert("Total dos produtos", msg, "OK");
+            DisplayAlert("Total dos produtos", msg, "OK");
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+
+
     }
 
-    private void MenuItem_Clicked(object sender, EventArgs e)
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
-        //ainda será implementado o remove
+
+    }
+
+    private async Task MenuItem_Clicked_1(object sender, EventArgs e)
+    {
+        try
+        {
+            MenuItem selecionado = sender as MenuItem;
+
+            Produto p = selecionado.BindingContext as Produto;
+
+            bool confirm = await DisplayAlert("Tem certeza que deseja Excluir?", "Remover Produto?", "Sim", "Não");
+
+            if (confirm)
+            {
+                await App.DB.Delete(p.Id);
+                listaFiltrada.Remove(p);
+                listaCompleta.Remove(p);
+            }
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
